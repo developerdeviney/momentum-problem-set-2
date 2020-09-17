0184b1c73ab9b7cd6d9c6bf605f8d785c2053ccf
jon deviney <developerdeviney1@gmail.com>
2020-09-16 20:21:22 -0600

    Prob set 2

--- homework.js
+++ homework.js
@@ -2,15 +2,25 @@
 // member of the array, and returns a new array with that member removed.
 // For example, `remove(['Cadence', 'Ordel', 'Marion'], 'Marion')` results
 // in `['Cadence', 'Ordel']`.
-//
 // If the potential member is not in the array, return the array unchanged.
 // If the potential member is in the array, remove all instances of it from the array.
+function remove (array, item) {
+    let indexOfItem = array.indexOf(item)
+    array.splice(indexOfItem, 2)
+    return array
+} 
+    
+
+
 
 // 2. Revisit your "remove" function. Make sure that it does not change the original
 // array but instead returns a new array.
 
+
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
+//}
+
 
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
